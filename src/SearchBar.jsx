37f9ec0f57095ec5e8b289a76c14ee7c5592cb20
Brenton Ring <brentonring@brentons-air.myfiosgateway.com>
2022-06-13 18:07:56 -0400

    without context

---
+++ src/SearchBar.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+export default function SearchBar(props) {
+    let [searchTerm, setSearchTerm] = useState('')
+
+    return(
+        <form onSubmit={(e) => props.handleSearch(e, searchTerm)}>
+            <input type='text' placeholder="Enter a search term here" onChange={(e) => props.handleSearch(e, e.target.value)} />
+            <input type='submit' />
+        </form>
+    )
+}
