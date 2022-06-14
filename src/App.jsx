fde3d63c6b32d7af72bfaeab729f15afbe27e661
Brenton Ring <brentonring@brentons-air.myfiosgateway.com>
2022-06-13 21:10:41 -0400

    refactored for redundancy

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,29 @@
-import { useEffect, useState, useRef } from 'react';
+import { useState, useRef } from 'react';
 import Gallery from './Gallery';
 import SearchBar from './SearchBar';
 import { DataContext } from './context/DataContext';
 import { SearchContext } from './context/SearchContext';
 
 function App() {
-  let [search, setSearch] = useState('')
   let [message, setMessage] = useState('Search for Music!')
   let [data, setData] = useState([])
   let searchInput = useRef('')
 
   const API_URL = 'https://itunes.apple.com/search?term='
 
-  useEffect(() => {
-    if(search) {
-      const fetchData = async() => {
-        document.title = `${search} Music`
-        const response = await fetch(API_URL + search)
-        const resData = await response.json()
-        if (resData.results.length > 0) {
-          setData(resData.results)
-        } else {
-          setMessage('Not Found')
-        }
-      }
-      fetchData()
-    }
-  }, [search])
-
   const handleSearch = (e, term) => {
     e.preventDefault()
-    setSearch(term)
+    const fetchData = async() => {
+      document.title = `${term} Music`
+      const response = await fetch(API_URL + term)
+      const resData = await response.json()
+      if (resData.results.length > 0) {
+        setData(resData.results)
+      } else {
+        setMessage('Not Found')
+      }
+    }
+    fetchData()
   }
 
   return (
